90679546afa4217c9809109ce30c6a1595a1b5a7
Jahir Jimenez <jahirjimenez@192.168.1.6>
2024-04-07 00:41:23 -0600

    Initial commit stable first version

---
+++ src/main/java/com/api/venta/JwtApplication.java
@@ -0,0 +1,27 @@
+package com.api.venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtApplication.class, args);
+	}
+
+	//Bean para permitir el acceso a la API desde cualquier origen
+	@Bean
+	WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
+
+}
---
+++ src/test/java/com/api/venta/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.venta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
